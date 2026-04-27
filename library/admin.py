2c850f20dc78c41e356b59fd5541d39a47a42d91
SamiSidar <freedomsami73@gmail.com>
2026-04-27 12:12:50 +0300

    feat: add Library, Zone, Seat, CheckIn models with admin

---
+++ library/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Library, Zone, Seat, CheckIn
+
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ("name", "opening_time", "closing_time")
+
+
+@admin.register(Zone)
+class ZoneAdmin(admin.ModelAdmin):
+    list_display = ("name", "library", "zone_type", "floor")
+    list_filter = ("library", "zone_type")
+
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ("code", "zone", "is_active", "is_occupied")
+    list_filter = ("zone", "is_active")
+
+
+@admin.register(CheckIn)
+class CheckInAdmin(admin.ModelAdmin):
+    list_display = ("user", "seat", "checked_in_at", "checked_out_at")
+    list_filter = ("seat__zone__library",)
